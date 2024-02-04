29e47caebc56efd08182156e98fb70d537b8eb08
Carlos Adriano <cadriano.miranda@gmail.com>
2024-02-04 12:00:35 -0300

    feat: add login page

---
+++ src/pages/login/Login.index.tsx
@@ -0,0 +1,26 @@
+import "./login.scss";
+
+function LoginPage() {
+  return (
+    <div className="login-brackground">
+      <h1 className="login-header">
+        Bem vindo a{" "}
+        <span style={{ color: "#52B788" }}>melhor consultoria online</span> da
+        sua vida
+      </h1>
+      <input
+        className="login-input"
+        placeholder="Digite seu email"
+        type="email"
+      />
+      <input
+        className="login-input"
+        placeholder="Digite sua senha"
+        type="password"
+      />
+      <button className="login-button">Login</button>
+    </div>
+  );
+}
+
+export default LoginPage;

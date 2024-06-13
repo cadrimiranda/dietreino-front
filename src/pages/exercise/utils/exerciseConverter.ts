aeef3552163423b7b61e65fea568b69ea8894cd6
Carlos Adriano Miranda <cadriano.miranda@gmail.com>
2024-06-13 15:16:34 -0300

    feat: create data converters

---
+++ src/pages/exercise/utils/exerciseConverter.ts
@@ -0,0 +1,36 @@
+import { MuscularGroupEnum } from "../../../utils/useMuscularGroupEnum";
+import {
+  Exercise,
+  ExerciseWithMuscularGroup,
+} from "../../users/components/gymWorkout/activeWorkout/workoutTypes";
+import { ExerciseForm } from "../components/ExerciseFormModal";
+
+type ExercisePostDTO = Omit<ExerciseForm, "muscularGroup"> & {
+  muscularGroup: MuscularGroupEnum;
+};
+
+export type ExercisePutDTO = Exercise & {
+  muscularGroup: MuscularGroupEnum;
+};
+
+export const exerciseFormToDTO = (data: ExerciseForm): ExercisePostDTO => data;
+
+export const exerciseToEditDTO = (
+  data: ExerciseForm & { id: string }
+): ExercisePutDTO => {
+  return {
+    ...data,
+    muscularGroup: data.muscularGroup,
+  };
+};
+
+export const exerciseToForm = (
+  exercise: ExerciseWithMuscularGroup
+): ExerciseForm => {
+  return {
+    ...exercise,
+    url: exercise.url || "",
+    image: exercise.image || "",
+    muscularGroup: exercise.muscularGroup,
+  };
+};

2d5661c20bab2dd23d29459b60396360c037892f
Carlos Adriano Miranda <cadriano.miranda@gmail.com>
2024-06-24 13:45:33 -0300

    style: refactor to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "color-primary": "#52B788",
+        "color-primary-lighter": "#c2fce1",
+        "color-primary-medium": "#40916C",
+        "color-primary-dark": "#1B4332",
+      },
+    },
   },
   plugins: [],
 };
